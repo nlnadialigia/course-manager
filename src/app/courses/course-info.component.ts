d14295ceb15ea0507e5092b3cbf77938eda6cffe
Nadia <nlnadialigia@gmail.com>
2021-07-04 15:04:30 -0300

    feat: create form and template in course-info

--- src/app/courses/course-info.component.ts
+++ src/app/courses/course-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
   templateUrl: './course-info.component.html'
@@ -7,12 +9,19 @@ import { ActivatedRoute } from '@angular/router';
 
 export class CourseInfoComponent implements OnInit{
   
-  courseId!: number;
+  course!: Course;
   
-  constructor(private activateRoute: ActivatedRoute) {}
+  constructor(
+    private activateRoute: ActivatedRoute, 
+    private courseService: CourseService
+    ) {}
 
   ngOnInit(): void {
-    this.courseId = +this.activateRoute.snapshot.paramMap.get('id')!;
+    this.course = this.courseService.retrieveById(+this.activateRoute.snapshot.paramMap.get('id')!);
+  }
+
+  save(): void {
+    this.courseService.save(this.course);
   }
 
 }
